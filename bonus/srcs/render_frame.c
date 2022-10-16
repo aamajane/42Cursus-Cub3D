b26c31771e82ffbb97f9d7c8e5bb319b81207c94
aamajane <aamajane@student.1337.ma>
2022-10-16 19:52:52 +0100

    fix door rendering with enemy

--- bonus/srcs/render_frame.c
+++ bonus/srcs/render_frame.c
@@ -6,7 +6,7 @@
 /*   By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 16:35:14 by aamajane          #+#    #+#             */
-/*   Updated: 2022/10/14 22:46:20 by aamajane         ###   ########.fr       */
+/*   Updated: 2022/10/16 19:49:27 by aamajane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ int	render_frame(t_data *data)
 	raycasting(data);
 	projection(data);
 	render_3d_projection(data);
+	render_enemy_projection(data);
 	if (data->door.render)
 		render_door(data);
-	render_enemy_projection(data);
 	render_weapon(data);
 	render_target(data);
 	mlx_put_image_to_window(data->mlx, data->win, data->main_img.addr, 0, 0);
